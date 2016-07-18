e0823249e049971f042a8abffba5c2c1904ad5e0
jconnolly171 <jconnolly171@ccvikings.com>
2016-07-18 15:40:55 -0400

    Very general pseudo-code for software planning

---
+++ sketch_jul18a/sketch_jul18a.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+/* Input rubix cube face colors into 2d array
+ *  Declare functions for all of the 18(?) possible moves 
+ *  From website determine algorithms that correspond with each starting possibility (different combinations of previously declared functions) 
+ *  Set if-statements for algorithms dependent upon positions in the 2d array 
+ *  Solve first layer and progress until eventual solution
+ */
+}
